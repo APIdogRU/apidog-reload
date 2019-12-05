ed45b3e70f7c7fd4a6ed89f8f38dbd7f56246fac
Vladislav Veluga <vladislav805@yandex.com>
2019-12-05 18:29:44 +0300

    Фиксы stylelint; допиливание конфигов webpack; вынос React во внешний бандл

--- .config/webpack.config.js
+++ .config/webpack.config.js
@@ -1,62 +1,98 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-const allowedModes = ['production', 'development'];
-const mode = allowedModes.includes(process.env.ENV) ? process.env.ENV : 'development';
+const isProduction = process.env.ENV === 'production';
 
+const mode = isProduction ? 'production' : 'development';
+
+const authDir = 'auth';
 const buildDir = 'build';
 
 module.exports = {
     mode,
 
-	entry: {
-        auth: path.resolve('src', 'auth', 'index.tsx'),
-		main: path.resolve('src', 'index.tsx'),
+    entry: {
+        auth: path.resolve('src', authDir, 'index.tsx'),
+        main: path.resolve('src', 'index.tsx'),
     },
 
-	output: {
-		path: path.resolve(buildDir),
-		filename: '[name].bundle.js',
+    output: {
+        path: path.resolve(buildDir),
+        filename: '[name].js'
     },
 
-	devServer: {
-		contentBase: path.resolve(buildDir),
+    devServer: {
+        contentBase: path.resolve(buildDir),
+        host: '0.0.0.0',
+        port: 8080,
     },
 
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.s?css$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-		],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s?css$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                exclude: /(node_modules)/,
+                use: ['file-loader'],
+            },
+        ],
     },
 
-	resolve: {
-		extensions: [ '.tsx', '.ts', '.js' ],
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
     },
 
-	plugins: [
-		new webpack.EnvironmentPlugin({
-			VERSION: process.env.npm_package_version
-		}),
-		new HtmlWebpackPlugin({
-			template: path.resolve('public', 'index.html'),
-			chunks: ['main'],
-			minify: true,
-			filename: 'index.html'
-		}),
-		new HtmlWebpackPlugin({
-			template: path.resolve('public', 'auth', 'index.html'),
-			chunks: ['auth'],
-			minify: true,
-			filename: 'login.html'
-		})
-	]
+    optimization: {
+        minimize: isProduction,
+        minimizer: [new TerserPlugin()],
+    },
+
+    externals: {
+        'react': 'React',
+        'react-dom' : 'ReactDOM'
+    },
+
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            VERSION: process.env.npm_package_version,
+            BUILD_DATE: (d => `${d.getFullYear()}${('00'+(d.getMonth()+1)).slice(-2)}${('00'+d.getDate()).slice(-2)}`)(new Date())
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve('public', 'index.html'),
+            chunks: ['main'],
+            minify: true,
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve('public', authDir, 'index.html'),
+            chunks: [authDir],
+            minify: true,
+            filename: 'auth.html'
+        })
+    ],
+
+    stats: {
+        children: false,
+    },
 };
